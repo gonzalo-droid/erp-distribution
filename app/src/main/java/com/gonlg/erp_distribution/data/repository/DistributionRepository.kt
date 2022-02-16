8faa51b92eba0cd11351dcc864140122b48f91d4
gonzalo-droid <gonzalolopezguerrero2050@gmail.com>
2022-02-16 10:25:45 -0500

    new structure in the repository with the preferences methods

--- app/src/main/java/com/gonlg/erp_distribution/data/repository/DistributionRepository.kt
+++ app/src/main/java/com/gonlg/erp_distribution/data/repository/DistributionRepository.kt
@@ -1,6 +1,7 @@
 package com.gonlg.erp_distribution.data.repository
 
 import com.gonlg.erp_distribution.data.Prefs
+import com.gonlg.erp_distribution.data.request.DistributionIdRequest
 import com.gonlg.erp_distribution.data.request.FilterDistributionRequest
 import com.gonlg.erp_distribution.data.response.*
 import com.gonlg.erp_distribution.data.retrofit.ApiService
@@ -12,10 +13,10 @@ import io.reactivex.schedulers.Schedulers
 class DistributionRepository(var apiService: ApiService, var prefs: Prefs) {
 
     fun getListDistribution(request: FilterDistributionRequest): Observable<DistributionResponse> {
-        val url = "${prefs.url.toString()}${URL_LIST_DISTRIBUTION}"
+        val url = url(prefs.url.toString(),URL_LIST_DISTRIBUTION )
         val headers = mapOf(
             "Content-Type" to "application/json",
-            "Authorization" to prefs.token.toString()
+            "Authorization" to token(prefs),
         )
 
         return apiService.getListDistribution(url, headers, request)
@@ -24,10 +25,10 @@ class DistributionRepository(var apiService: ApiService, var prefs: Prefs) {
     }
 
     fun getTowers(): Observable<List<TowerResponse>> {
-        val url = "${prefs.url.toString()}${URL_TOWER}"
+        val url = url(prefs.url.toString(),URL_TOWER )
         val headers = mapOf(
             "Content-Type" to "application/json",
-            "Authorization" to prefs.token.toString()
+            "Authorization" to token(prefs),
         )
 
         return apiService.getTowers(url, headers)
@@ -36,10 +37,10 @@ class DistributionRepository(var apiService: ApiService, var prefs: Prefs) {
     }
 
     fun getLevels(): Observable<List<LevelResponse>> {
-        val url = "${prefs.url.toString()}${URL_LEVEL}"
+        val url = url(prefs.url.toString(),URL_LEVEL )
         val headers = mapOf(
             "Content-Type" to "application/json",
-            "Authorization" to prefs.token.toString()
+            "Authorization" to token(prefs),
         )
 
         return apiService.getLevels(url, headers)
@@ -48,10 +49,10 @@ class DistributionRepository(var apiService: ApiService, var prefs: Prefs) {
     }
 
     fun getArounds(): Observable<List<AroundResponse>> {
-        val url = "${prefs.url.toString()}${URL_AROUND}"
+        val url = url(prefs.url.toString(),URL_AROUND )
         val headers = mapOf(
             "Content-Type" to "application/json",
-            "Authorization" to prefs.token.toString()
+            "Authorization" to token(prefs),
         )
 
         return apiService.getArounds(url, headers)
@@ -62,10 +63,10 @@ class DistributionRepository(var apiService: ApiService, var prefs: Prefs) {
     }
 
     fun getProjects(): Observable<List<ProjectResponse>> {
-        val url = "${prefs.url.toString()}${URL_PROJECT}"
+        val url = url(prefs.url.toString(),URL_PROJECT )
         val headers = mapOf(
             "Content-Type" to "application/json",
-            "Authorization" to prefs.token.toString(),
+            "Authorization" to token(prefs),
             "Connection" to "close"
         )
 
@@ -76,14 +77,13 @@ class DistributionRepository(var apiService: ApiService, var prefs: Prefs) {
 
     companion object {
 
+        fun token(prefs: Prefs) = prefs.token.toString()
+        fun url(url: String, endpoint: String) = "${url}${endpoint}"
+
         val URL_LIST_DISTRIBUTION =  "/report-distribution/filter"
-
         val URL_LEVEL =  "/all_levels"
-
         val URL_AROUND =  "/all_arounds"
-
         val URL_PROJECT =  "/all_projects"
-
         val URL_TOWER =  "/all_towers"
 
 
--- app/src/main/java/com/gonlg/erp_distribution/data/repository/SaleDetailRepository.kt
+++ app/src/main/java/com/gonlg/erp_distribution/data/repository/SaleDetailRepository.kt
@@ -1,11 +1,9 @@
 package com.gonlg.erp_distribution.data.repository
 
-import android.util.Log
 import com.gonlg.erp_distribution.data.Prefs
 import com.gonlg.erp_distribution.data.request.DistributionIdRequest
 import com.gonlg.erp_distribution.data.response.SaleDetailResponse
 import com.gonlg.erp_distribution.data.retrofit.ApiService
-import com.gonlg.erp_distribution.utils.PapersManager
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -13,22 +11,22 @@ import io.reactivex.schedulers.Schedulers
 class SaleDetailRepository (var apiService: ApiService, var prefs: Prefs) {
 
     fun getSaleDetail(request: DistributionIdRequest): Observable<SaleDetailResponse> {
-        val url = "${prefs.url.toString()}${URL_SALE}?distribution_id=${request.distributionId}"
+        val url = url(prefs.url.toString(),request)
         val headers = mapOf(
             "Content-Type" to "application/json",
-            "Authorization" to prefs.token.toString(),
+            "Authorization" to token(prefs),
             "Cache-Control" to "no-cache"
         )
-        Log.d("Repo->",request.toString())
         return apiService.getDetailSale(url, headers)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
 
     companion object {
+        fun token(prefs: Prefs) = prefs.token.toString()
 
-        val URL_SALE =  "${PapersManager.login.dataCompany.url}/sale-first"
-
+        fun url(url: String,request: DistributionIdRequest) =
+            "${url}/sale-first?distribution_id=${request.distributionId}"
 
     }
 }
